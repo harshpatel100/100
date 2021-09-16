428c2c6e82836f2803b7ba3dc1e942204152ea4a
harshmpatel1 <patelharsh7458@gmail.com>
2021-09-16 15:25:23 +0530

    nice

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom'
+//var React = require('react');
+//var ReactDOM = require('react-dom');
+
+ReactDOM.render(<h1>Hellow , world</h1>,
+  document.getElementById('root')
+  );
